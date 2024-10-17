a25e936c54ea4545c03d1385cc4404e01a058665
iyan <andi.ilhamsyah@digi-id.id>
2024-10-17 10:45:48 +0700

    feat: updated frame and badge also added badge style

--- lib/src/avatar/frame.dart
+++ lib/src/avatar/frame.dart
@@ -1,24 +1,71 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_status_avatar/flutter_status_avatar.dart'
+    as flutter_status_avatar;
 
-class Frame extends StatefulWidget {
+///
+/// A class that provides frame avatar for [flutter_status_avatar.FlutterStatusAvatar]
+/// Using [CircleAvatar] for display the frame
+///
+class Frame extends StatelessWidget {
   const Frame({
     super.key,
-    required this.child,
+    this.backgroundColor,
+    this.backgroundImage,
+    this.radius,
+    this.minRadius,
+    this.maxRadius,
+    this.child,
   });
 
-  final Widget child;
+  /// The color with which to fill the circle. Changing the background color
+  /// will cause the avatar to animate to the new color
+  ///
+  /// If a [backgroundColor] is not specified and [ThemeData.useMaterial3] is true,
+  final Color? backgroundColor;
 
-  @override
-  State<Frame> createState() => _FrameState();
-}
+  /// The background image of the circle. Changing the background image
+  /// will cause the avatar to animate to the new image
+  ///
+  /// If the avatar is to have the user's initials, use [child] instead.
+  final ImageProvider<Object>? backgroundImage;
 
-class _FrameState extends State<Frame> {
+  /// The size of the avatar, expressed as the radius
+  ///
+  /// if [radius] is specified, then neither [minRadius] nor [maxRadius] may be specified.
+  final double? radius;
+
+  /// The minimum size of the avatar, expressed as the radius
+  ///
+  /// If [minRadius] is specified, then [radius] must not also be specified
+  /// Defaults to zero
+  final double? minRadius;
+
+  /// The maximum size of the avatar, expressed as the radius
+  ///
+  /// If [maxRadius] is specified, then [radius] must not also be specified
+  /// Defaults to [double.infinity]
+  final double? maxRadius;
+
+  /// The widget below this widget in the tree
+  ///
+  /// Typically a [Text] widget. If the avatar is to have an image, use [backgroundImage] instead
+  final Widget? child;
+
   @override
   Widget build(BuildContext context) {
     return _getFrame();
   }
 
   Widget _getFrame() {
-    return CircleAvatar();
+    /// See also
+    /// * [CircleAvatar]
+    ///
+    return CircleAvatar(
+      backgroundColor: backgroundColor,
+      backgroundImage: backgroundImage,
+      minRadius: minRadius,
+      maxRadius: maxRadius,
+      child: child,
+    );
   }
 }
--- lib/src/badge/badge.dart
+++ lib/src/badge/badge.dart
@@ -1,10 +1,53 @@
+import 'package:badges/badges.dart' as badges;
 import 'package:flutter/material.dart';
+import 'package:flutter_status_avatar/flutter_status_avatar.dart'
+    as flutter_status_avatar;
+import 'package:flutter_status_avatar/src/badge/badge_style.dart';
 
+/// A class that provides additional marker for [flutter_status_avatar.FlutterStatusAvatar]
+/// Using [badges.Badge]
+///
 class Badge extends StatelessWidget {
-  const Badge({super.key});
+  const Badge({
+    super.key,
+    this.badgeContent,
+    this.badgeStyle = const BadgeStyle(),
+    this.onTap,
+    this.child,
+  });
+
+  /// The widget inside [Badge]
+  final Widget? badgeContent;
+
+  /// The class that provides styling [Badge]
+  final BadgeStyle badgeStyle;
+
+  /// The function to adding action when [Badge] is pressed
+  /// The additional padding will be applied to Make the full [Badge] clickable
+  /// if the [Badge] is outside of the child
+  final Function()? onTap;
+
+  /// The widget below this [Badge] in the tree
+  final Widget? child;
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    /// See also
+    /// * [badges.Badge]
+    return badges.Badge(
+      badgeContent: badgeContent,
+      badgeStyle: badges.BadgeStyle(
+        shape: badgeStyle.shape,
+        borderRadius: badgeStyle.borderRadius,
+        badgeColor: badgeStyle.badgeColor,
+        borderSide: badgeStyle.borderSide,
+        elevation: badgeStyle.elevation,
+        badgeGradient: badgeStyle.badgeGradient,
+        borderGradient: badgeStyle.borderGradient,
+        padding: badgeStyle.padding,
+      ),
+      onTap: onTap,
+      child: child,
+    );
   }
 }
